de3cf78b8136dfd4ab64d25526b6d087d91ebc57
Bastiaan Stroosnijder <bas@strootje.com>
2025-03-15 14:53:52 +0100

    feat: lets just go with it..

--- README.md
+++ README.md
@@ -5,29 +5,25 @@
 
 Plausible provider for your [SolidJS](https://solidjs.com) app.
 
-All credit should go to [barbapapazes](https://github.com/barbapapazes) for updating the [plausible-tracker](https://github.com/barbapapazes/plausible-tracker)
+All credit should go to [barbapapazes](https://github.com/barbapapazes) for
+updating the
+[plausible-tracker](https://github.com/barbapapazes/plausible-tracker)
 
 ## Usage
-
-Install with [pnpm](https://pnpm.io/) (`solid-js` should be installed as a dev dependency):
-
-```sh
-pnpx jsr add -D @strootje/solid-plausible
-```
 
 Add the `PlausibleProvider` to your app component:
 
 ```tsx
 export default function App() {
-   return (
-      <PlausibleProvider>
-         // ...
-      </PlausibleProvider>
-	);
+  return (
+    <PlausibleProvider>
+      // ...
+    </PlausibleProvider>
+  );
 }
 ```
 
 ## License
 
-Licensed under the [MPL-2.0](LICENSE) license.<br/>
-Copyright &copy; 2024, Bastiaan Stroosnijder
+Licensed under the [MPL-2.0](LICENSE) license.<br/> Copyright &copy; 2024,
+Bastiaan Stroosnijder
--- mod.ts
+++ mod.ts
@@ -1,1 +1,53 @@
-export * from "./src/PlausibleContext";
+import {
+  createPlausibleTracker,
+  type EventOptions,
+  type Plausible,
+  type PlausibleOptions,
+} from "@barbapapazes/plausible-tracker";
+import { useAutoOutboundTracking as _useAutoOutboundTracking } from "@barbapapazes/plausible-tracker/extensions/auto-outbound-tracking";
+import { useAutoPageviews as _useAutoPageviews } from "@barbapapazes/plausible-tracker/extensions/auto-pageviews";
+import { createContext, onCleanup, useContext } from "solid-js";
+import { isServer } from "solid-js/web";
+
+const Context = createContext<Partial<PlausibleOptions>>();
+export const PlausibleProvider = Context.Provider;
+
+let plausible: Plausible;
+const usePlausible = () =>
+  plausible ??= createPlausibleTracker(useContext(Context));
+
+export const useTrackEvent = (
+  args: Parameters<typeof plausible.trackEvent>,
+) => {
+  if (isServer) return;
+  usePlausible().trackEvent(...args);
+};
+
+export const useTrackPageview = (
+  args: Parameters<typeof plausible.trackPageview>,
+) => {
+  if (isServer) return;
+  usePlausible().trackPageview(...args);
+};
+
+export const useAutoOutboundTracking = (opts?: EventOptions) => {
+  if (isServer) return;
+  const { cleanup, install } = _useAutoOutboundTracking(usePlausible(), opts);
+
+  try {
+    install();
+  } finally {
+    onCleanup(cleanup);
+  }
+};
+
+export const useAutoPageviews = (opts?: EventOptions) => {
+  if (isServer) return;
+  const { cleanup, install } = _useAutoPageviews(usePlausible(), opts);
+
+  try {
+    install();
+  } finally {
+    onCleanup(cleanup);
+  }
+};
